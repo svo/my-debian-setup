ba586cde5741c926d3618737fc871f128b01feda
Sean Van Osselaer <svo@svo-desktop>
2018-02-17 23:28:35 +1100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+sudo apt-get install -y ansible
+
+ansible-playbook -i "localhost," -c local -K playbook.yml
+
+sudo apt -y autoremove
