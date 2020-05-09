d907d26a635b3142f63f56b3223956fff52ba461
svo <svo@qual.is>
2020-05-10 05:15:55 +1000

    Correction to pip upgrade in build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get install -y python3-pip
 
-sudo -H pip3 install --upgrade pip3
+sudo -H pip3 install --upgrade pip
 sudo -H pip3 install ansible
 
 ansible-playbook -i "localhost," -c local -K playbook.yml
