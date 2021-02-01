a3ba162a73de9dcf5882af4748b3039e83377b00
SVO <svo@qual.is>
2021-02-02 06:08:48 +1100

    Ensuring python3 is used by ansible

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ sudo apt-get install -y python3-pip
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install ansible
 
-ansible-playbook -i "localhost," -c local -K playbook.yml
+ansible-playbook -e "ansible_python_interpreter:'/usr/bin/python3'" -i "localhost," -c local -K playbook.yml
 
 sudo apt -y autoremove
